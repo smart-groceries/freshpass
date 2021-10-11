5f47301823a455260509191931217d3a7d753b51
Jenish <50473177+Jenishhd@users.noreply.github.com>
2021-10-10 20:04:57 -0700

    Changed GroceryItem Component Design and Format
    Still working on navigation and payment

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -23,36 +23,43 @@ import {
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+
 import ForgotPasswordScreen from './src/screens/ForgotPassword';
 import CreatAccountScreen from './src/screens/CreateAccount';
-import LoginScreen from './src/screens/LoginScreen';
+import LoginScreen from './src/screens/LoginScreen'
 import ShoppingLists from './src/screens/ShoppingLists';
 import NavBar from './src/components/NavBar';
-import PaymentConfirmation from './src/screens/PaymentConfirmation';
-import HomePage from './src/screens/HomePage';
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import Payments from './src/screens/Payments';
 import AddPayment from './src/screens/AddPayment';
 import CartView from './src/screens/CartView';
 import EditItem from './src/screens/EditItem';
-import OrderRejected from './src/screens/OrderRejected';
-import {AppRegistry} from 'react-native';
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import { AppRegistry } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import LandingPage from './src/screens/LandingPage';
+import HomePage from './src/screens/HomePage';
+
+
+
 
 // Initialize Apollo Client
 const client = new ApolloClient({
   uri: 'localhost:4000/graphql',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache()
 });
+
+
 
 const StoreStack = createStackNavigator();
 
 function StoreStackScreen() {
+  
   return (
     <StoreStack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
+    screenOptions={{
+      headerShown:false
+    }}>
       <StoreStack.Screen name="Home" component={AddPayment} />
     </StoreStack.Navigator>
   );
@@ -63,10 +70,10 @@ const AccountStack = createStackNavigator();
 function AccountStackScreen() {
   return (
     <AccountStack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <AccountStack.Screen name="Account" component={PaymentConfirmation} />
+    screenOptions={{
+      headerShown:false
+    }}>
+      <AccountStack.Screen name="Account" component={EditItem} />
     </AccountStack.Navigator>
   );
 }
@@ -74,15 +81,17 @@ function AccountStackScreen() {
 const ShoppingListsStack = createStackNavigator();
 
 function ShoppingListsStackScreen() {
-  return (
+  return(
     <ShoppingListsStack.Navigator
-      screenOptions={{
-        headerShown: true,
-      }}>
-      <ShoppingListsStack.Screen name="Cart" component={OrderRejected} />
+    screenOptions={{
+      headerShown:true
+    }}
+    >
+      <ShoppingListsStack.Screen name ="Cart" component = {CartView}/>
     </ShoppingListsStack.Navigator>
-  );
+  )
 }
+
 
 const Tab = createBottomTabNavigator();
 
@@ -90,32 +99,44 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Tab.Screen
-          name="Account"
+      screenOptions={{
+        headerShown: false
+      }}
+
+      
+      >
+        <Tab.Screen name="Account" 
           component={StoreStackScreen}
           // options= {{
           //   tabBarButton: (props) =><NavBar/>,
           // }}
         />
-
-        <Tab.Screen
-          name="Settings"
+        
+        <Tab.Screen name="Settings" 
           component={AccountStackScreen}
           // options= {{
           //   tabBarButton: (props) => <NavBar/>
-          // }}
+          // }} 
         />
 
-        <Tab.Screen
-          name="Shopping Lists"
+        {/* <Tab.Screen name="Shopping Lists" 
           component={ShoppingListsStackScreen}
           // options= {{
           //   tabBarButton: (props) => <NavBar/>
-          // }}
-        />
+          // }} 
+        /> */}
+
+        <Tab.Screen
+                name="Cart View"
+                component={ShoppingListsStackScreen}
+                options={{
+                  tabBarLabel: 'Home',
+                  
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name="account" color= "black" size={26} />
+                  ),
+                }}
+              />
       </Tab.Navigator>
     </NavigationContainer>
   );
