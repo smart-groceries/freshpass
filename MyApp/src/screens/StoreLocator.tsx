db1336fb09b6204d52cdd4427e721057e7c8f981
Matthew Ortiz <58668605+matthewo22@users.noreply.github.com>
2021-11-12 18:53:57 -0800

    Google Places Update
    google places update to store locator

--- MyApp/src/screens/StoreLocator.tsx
+++ MyApp/src/screens/StoreLocator.tsx
@@ -2,10 +2,61 @@ import * as React from 'react';
 import MapView, { Callout, Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { renderToStringWithData } from '@apollo/client/react/ssr';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 export default function StoreLocator() {
+  const [ region, setRegion ] = React.useState({
+    latitude: 33.7838,
+    longitude: -118.1141,
+    latitudeDelta: 0.015,
+    longitudeDelta: 0.04
+  })
+  const currentLocation = {
+    description: 'Current Location',
+    geometry: { location: { lat:   33.785056718140424, lng: -118.11488944704615 } },
+  };
+  const myStore = {
+    description: 'My Store',
+    geometry: { location: { lat:   33.7635316460389, lng: -118.1158628065532 } },
+  };
+  
+
+  const GOOGLE_PLACES_API_KEY = 'AIzaSyC_eCHatI834lJ-pvFs9qxjSS-Bu-iFVQE';
+  
   return (
     <View style={styles.container}>
+     <GooglePlacesAutocomplete
+        placeholder="Search"
+        fetchDetails = {true}
+        GooglePlacesDetailsQuery={{
+          rankby: "distance"
+        }}
+        query={{
+          key: GOOGLE_PLACES_API_KEY,
+          language: 'en', // language of the results
+          // components: "country: us",
+          // types: "supermarket",
+          //radius: 30000,
+          location: `${region.latitude}, ${region.longitude}`
+
+        }}
+        onPress={(data, details = null) => {
+          console.log(data, details)
+          setRegion({
+            latitude: details.geometry.location.lat,
+            longitude: details.geometry.location.lng,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.04
+          })
+        }}
+        predefinedPlaces={[currentLocation, myStore]}
+
+        onFail={(error) => console.error(error)}
+        styles = {{
+          container: {flex: 0, position: "absolute", width: "100%", zIndex: 1},
+          listView: { backgroundColor: "white"}
+        }}
+      />
       <MapView style={styles.map}
       initialRegion={{
         latitude: 33.7838,
@@ -34,17 +85,19 @@ export default function StoreLocator() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
   map: {
     width: Dimensions.get('window').width,
-    height: 700
+    height: 690
   },
   storename: {
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  nav: {
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
+
