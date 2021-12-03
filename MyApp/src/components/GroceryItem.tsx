86db8087c283c06d01fa68f663b7c0ed6e94f2a5
Drew Gomez <drew.gomez27@gmail.com>
2021-12-02 22:22:06 -0800

    updated the style for the item component

--- MyApp/src/components/GroceryItem.tsx
+++ MyApp/src/components/GroceryItem.tsx
@@ -4,7 +4,6 @@ import { FC } from 'react';
 import NumericInput from 'react-native-numeric-input';
 
 
-
 import {
   View,
   Image,
@@ -24,20 +23,26 @@ interface Props{
 }
 
 const Item :FC<Props> = () => {
-    const [testName, setTestName] = React.useState("Kraft");
-    const [name, setName] = React.useState<string | null>(null);
-    const [brand, setBrand] = React.useState<string | null>(null);
-    const [price, setPrice] = React.useState<number | null>(null);
-    const [image, setImage] = React.useState<Image | null>(null);
+    const [name, setName] = React.useState("Mac and Cheese");
+    const [weight, setWeight] = React.useState("0.5");
+    const [brand, setBrand] = React.useState("Kraft");
+    const [price, setPrice] = React.useState(".99");
+    const [aisle, setAisle] = React.useState("1");
+    const [imageUrl, setimageUrl] = React.useState("https://smart-grocery-project.s3.us-west-1.amazonaws.com/item-1.jpeg");
     const [quantityValue, setQuantityValue] = React.useState<number>(0);
+
+    
+
     return(
-        
         <View style={styles.GroceryItemContainer}>
                 <View style={styles.LeftContainer} >
-                    <Text> {testName} </Text>
+                    <Text style={styles.itemTitle}>{name}</Text>
+                    <Text style={styles.itemBrand}>{brand}</Text>
                     <View style={styles.priceContainer}>
-                        <Text>$5 ~ </Text>
-                        <Text>Aisle 1</Text>
+                        <Text style={styles.itemPrice}>$ </Text>
+                        <Text style={styles.itemPrice}>{price}</Text>
+                        <Text style={styles.itemPriceDivider}> • </Text>
+                        <Text style={styles.itemAisle}>Aisle 1</Text>
                     </View>
                     <View style ={styles.QuantityButtonContainer}>
                         <NumericInput value={quantityValue} onChange={value => setQuantityValue(value)} totalHeight= {30} totalWidth= {70}
@@ -63,13 +68,13 @@ const Item :FC<Props> = () => {
                         {/* <TouchableHighlight>
                             <Image resizeMode='contain' source={require('./assets/filter_icon.png')}/>
                         </TouchableHighlight> */}
-                    </View>
+                    </View>  
                 </View>
                 <View style={styles.ItemPicContainer}>
                     <Image
                     style={styles.image}
                     resizeMode="contain"
-                    source={require('./../assets/macncheese.png')}>
+                    source={{uri: imageUrl}}>
                     </Image>
                 </View>
         </View>
@@ -83,9 +88,9 @@ const styles = StyleSheet.create({
     GroceryItemContainer:{
         width:"100%",
         backgroundColor:'transparent',
-        padding:10,
+        padding:20,
         flexDirection:'row',
-        fontFamily: 'varelaround-regular',
+        fontFamily: 'VarelaRound-Regular',
         marginVertical: 5,
     },
     ItemControllerContainer:{
@@ -95,19 +100,20 @@ const styles = StyleSheet.create({
     QuantityButtonContainer:{
         backgroundColor:'transparent',
         flexDirection:"row",
-        fontFamily: 'varelaround-regular',
+        fontFamily: 'VarelaRound-Regular',
     },
     LeftContainer:{
       flex:1,  
       backgroundColor:'transparent',
-      fontFamily: 'varelaround-regular',
+      fontFamily: 'VarelaRound-Regular',
       paddingVertical: 10,
       paddingBottom: 10
     },
     priceContainer:{
         backgroundColor:'transparent',
         flexDirection:"row",
-        fontFamily: 'varelaround-regular',
+        fontFamily: 'VarelaRound-Regular',
+        marginTop: 4,
         paddingBottom: 10
         
     },
@@ -119,6 +125,34 @@ const styles = StyleSheet.create({
     },
     image:{
         flex:1,
+        width: 125,
+        height: 125,
         alignSelf:'flex-end'
+    },
+    itemTitle: {
+        fontSize: 16,
+        color: '#424347',
+        fontFamily: 'VarelaRound-Regular'
+    },
+    itemBrand: {
+        fontSize: 13,
+        color: '#BBBBBB',
+        fontFamily: 'VarelaRound-Regular',
+        marginTop: 4
+    },
+    itemPrice: {
+        fontSize: 14,
+        color: '#424347',
+        fontFamily: 'VarelaRound-Regular'
+    },
+    itemPriceDivider: {
+        fontSize: 14,
+        color: '#D8D8D8',
+        fontFamily: 'VarelaRound-Regular'
+    },
+    itemAisle: {
+        fontSize: 14,
+        color: '#E89023',
+        fontFamily: 'VarelaRound-Regular'
     }
 });
