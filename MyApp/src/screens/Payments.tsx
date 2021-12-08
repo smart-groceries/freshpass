f7cc4551ad9fce7afb3ce09e8952c1d69609056a
Andrew Baltazar <baltazarcode@gmail.com>
2021-12-07 22:09:05 -0800

    Fixed payment method loading indicator centering

--- MyApp/src/screens/Payments.tsx
+++ MyApp/src/screens/Payments.tsx
@@ -299,8 +299,8 @@ const styles = StyleSheet.create({
   },
   loading: {
     position: 'absolute',
-    // left: 0,
-    // right: 0,
+    left: 0,
+    right: 0,
     top: 0,
     bottom: 0,
     alignItems: 'center',
